2403c911157b87837242d7e6e2db31df6c76e33a
Satya Deepu Mandapati <122634830+s555842@users.noreply.github.com>
2023-04-05 16:24:48 -0500

    Delete routes directory

--- routes/student.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Student', { title: 'Search Results for Student' });
-});
-
-module.exports = router;
