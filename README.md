439d3dbc46c419db66e9443112f5a9c5eb2ebbc1
s555842 <122634830+s555842@users.noreply.github.com>
2023-04-05 16:34:38 -0500

    updated

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Remote repo for dynamic pages testing
 
 Render URL:<https://s23wb78mandapati.onrender.com>
 
-Id with my class "Student"  with attributes Stu_Name(String),Stu_Age(number),Mail_Id(String)
+Id with my class is "Student"  with attributes Stu_Name(String),Stu_Age(number),Mail_Id(String)
--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var studentRouter = require('./routes/student');
+var boardRouter= require('./routes/Board');
+var selectorRouter = require('./routes/Selector');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/student', studentRouter);
+app.use('/selector', selectorRouter);
+app.use('/board', boardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
